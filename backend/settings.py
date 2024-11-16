b3cc4f7ccb823e30092a14de8f3a06c5fd5e43fd
Tyler <tylersora@gmail.com>
2024-11-16 08:19:41 +0000

    Add postgres database to settings

--- backend/settings.py
+++ backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smart_task_db',
+        'USER': 'smart_task_user',
+        'PASSWORD': '1234qwer',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
