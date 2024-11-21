acdd3fea308848e5a00aba4deeb3d5bf109363f2
Tyler <tylersora@gmail.com>
2024-11-21 01:47:35 +0000

    Add navbar and more instructions on readme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TaskList from './components/TaskList';
 import AddTask from './components/AddTask';
+import Navbar from './components/Navbar'; // Import the Navbar component
 
-const App = () => {
+function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<TaskList />} />
-                <Route path="/add-task" element={<AddTask />} />
-            </Routes>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<h1>Welcome to Smart Task Manager</h1>} />
+                    <Route path="/tasks" element={<TaskList />} />
+                    <Route path="/add-task" element={<AddTask />} />
+                    <Route path="/about" element={<h1>About This Project</h1>} />
+                </Routes>
+            </div>
         </Router>
     );
-};
+}
 
 export default App;
